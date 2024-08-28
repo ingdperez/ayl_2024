85d4c0bd739672e2f35845c9ce604c0c9321e089
dperez-incomm <dperez@incomm.com>
2024-08-28 08:22:08 -0300

    first commit

---
+++ p1/e6.py
@@ -0,0 +1,37 @@
+import re
+
+
+def validate_a(value: str) -> bool:
+    return False
+
+
+def validate_b(value: str) -> bool:
+    return False
+
+
+def validate_c(value: str) -> bool:
+    return False
+
+
+def validate_d(value: str) -> bool:
+    return False
+
+
+def validate_e(value: str) -> bool:
+    return False
+
+
+def validate_g(value: str) -> bool:
+    return False
+
+
+def validate_f(value: str) -> bool:
+    return False
+
+
+def validate_g(value: str) -> bool:
+    return False
+
+
+if __name__ == '__main__':
+    print(validate_e("127.0.0.1\n"))
---
+++ p1/e7.py
@@ -0,0 +1,14 @@
+import re
+
+
+def convert(input_file_path: str, output_file_path: str) -> None:
+    """
+    :param input_file_path: csv input file
+    :param output_file_path: csv output file
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    convert("MOCK_DATA.csv", "MOCK_DATA_RESULT.csv")
